6ec7caa71c5c841891ad62509affd55634cfe742
EmmaJepson <emma.jepson9@gmail.com>
2024-06-17 13:15:04 -0400

    added stuff

--- ai.py
+++ ai.py
@@ -4,7 +4,7 @@ import pandas as pd
 from PIL import Image
 from keras.utils.image_utils import img_to_array
 
-model= keras.models.load_model("D:\\Repos\\REU Summer 2024\\archive\\EfficientNetB0-525-(224 X 224)- 98.97.h5", custom_objects={'F1_score':'F1_score'})
+model= keras.models.load_model("C:\\Users\\acosc\\REU_Summer_2024\\EfficientNetB0-525-(224 X 224)- 98.97.h5", custom_objects={'F1_score':'F1_score'})
 
 firstcsvline = True
 dataframe = pd.read_csv('birds.csv', header = None, names = ['class id', 'image_path', 'labels', 'data set', 'scientific name'])
@@ -13,8 +13,10 @@ for file_path in dataframe['image_path']:
     if firstcsvline:
         firstcsvline = False
         continue
-    image = Image.open(file_path)
-    image_array = img_to_array(image)
-    images.append(image_array)
+    if file_path.startswith('test/'):
+        image = Image.open(file_path)
+        image_array = img_to_array(image)
+        images.append(image_array)
 
 input_data = np.array(images)
+print(input_data[1][1])
