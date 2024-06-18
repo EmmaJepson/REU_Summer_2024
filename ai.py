409b770241b6e8f477d48269dd73567c514b9f51
EmmaJepson <emma.jepson9@gmail.com>
2024-06-18 06:14:38 -0400

    added more stuff

--- ai.py
+++ ai.py
@@ -3,11 +3,12 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 from keras.utils.image_utils import img_to_array
+from keras.applications.efficientnet import preprocess_input
 
-model= keras.models.load_model("C:\\Users\\acosc\\REU_Summer_2024\\EfficientNetB0-525-(224 X 224)- 98.97.h5", custom_objects={'F1_score':'F1_score'})
+model = keras.models.load_model("D:\\Repos\\REU_Summer_2024\\EfficientNetB0-525-(224 X 224)- 98.97.h5", custom_objects={'F1_score':'F1_score'})
 
 firstcsvline = True
-dataframe = pd.read_csv('birds.csv', header = None, names = ['class id', 'image_path', 'labels', 'data set', 'scientific name'])
+dataframe = pd.read_csv('D:\\Repos\\REU_Summer_2024\\birds.csv', header = None, names = ['class id', 'image_path', 'labels', 'data set', 'scientific name'])
 images = []
 for file_path in dataframe['image_path']:
     if firstcsvline:
@@ -15,8 +16,31 @@ for file_path in dataframe['image_path']:
         continue
     if file_path.startswith('test/'):
         image = Image.open(file_path)
+        image = image.resize((224, 224))
         image_array = img_to_array(image)
+        image_array = preprocess_input(image_array)
         images.append(image_array)
 
 input_data = np.array(images)
-print(input_data[1][1])
+increment = 0
+pred = model.predict(input_data)
+pred_classes = np.argmax(pred, axis = 1)
+class_labels = dataframe['class id'].unique()
+'''for file_path in dataframe['image_path']:
+    if firstcsvline:
+        firstcsvline = False
+        continue
+    if file_path.startswith('test/'):
+        increment += 1
+        x1 = dataframe.loc[file_path, 'file_path']
+        print(x1)
+        x2 = dataframe.loc[increment, 'class id']
+        print(x1)
+        x3 = class_labels[pred_classes[increment]]'''
+
+
+for i, pred_class in enumerate(pred_classes):
+    x1 = dataframe[dataframe['image_path'].str.startswith('test/')].iloc[i]['image_path']
+    x2 = dataframe[dataframe['image_path'].str.startswith('test/')].iloc[i]['class id']
+    x3 = class_labels[pred_class]
+    print(f"Image: {x1}, Predicted: {x2}, Actual: {x3}")
